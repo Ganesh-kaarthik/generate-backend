881b0b604fd12c2e8d310235f47e19b88226eb90
Ganesh-kaarthik <110884217+Ganesh-kaarthik@users.noreply.github.com>
2023-02-23 17:06:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,20 @@ import dalleRoutes from './routes/dalleRoutes.js';
 dotenv.config();
 
 const app = express();
-app.use(cors(https://golden-yeot-b2514f.netlify.app));
+app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header(
+      "Access-Control-Allow-Headers",
+      "x-auth-token, Origin, Content-Type, Accept"
+  );
+  res.setHeader("Access-Control-Allow-Origin","*");
+  res.setHeader("Access-control-Allow-Methods", "GET, POST, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Origin,X-Requested-With, content-type,Accept,Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+
 app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/v1/post', postRoutes);
